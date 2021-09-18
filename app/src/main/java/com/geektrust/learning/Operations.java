387ff1ee79ce10bce882807f0d69c6010c6e14af
ajinkyagh <ajinkyagh@gmail.com>
2021-09-18 19:31:48 +0530

    Adds Operations and OperationPerformer classes

--- app/src/main/java/com/geektrust/learning/Operations.java
+++ app/src/main/java/com/geektrust/learning/Operations.java
@@ -1,7 +1,21 @@
 package com.geektrust.learning;
 
 public class Operations {
+    OperationsPerformer operationsPerformer = new OperationsPerformer();
+    String[] splitInput;
+    String option;
     public void select(String input) {
-        System.out.println(input);
+        splitInput = input.split("\\s");
+        option=splitInput[0];
+        switch (option){
+            case "BOOK":
+                operationsPerformer.bookRoom(splitInput);
+            break;
+
+            case "VACANCY":
+                operationsPerformer.roomVacancy(splitInput);
+            break;
+
+        }
     }
 }
